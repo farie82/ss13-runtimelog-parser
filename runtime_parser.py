5961887246467d6f7577c7e4e53d1f3b31057196
joep van der velden <joepvelden@hotmail.com>
2020-02-13 10:56:30 +0100

    Added more configurability and config checks

--- runtime_parser.py
+++ runtime_parser.py
@@ -1,20 +1,32 @@
 # Need PyGithub
 import sys
 import re
+import traceback
 from github import Github
 from datetime import datetime
 
 # Used to dynamically get issues (workaround for the 1000 issue retrieve limit)
 ISSUES_SINCE = datetime.strptime("01-01-2020", "%d-%m-%Y")
-
 class Parser:
     def load_config(self):
+        self.CENSORING_MATCH_RE = []
+        self.CENSORING_REPLACE_RE = []
+        self.ISSUE_TITLE_BASE = ""
+        self.ISSUE_CREATE_BODY = ""
         try:
             with open('./config/config.txt', 'r') as f:
                 for line in f:
                     self.parse_config(line)
         except Exception as e:
             print("Failed to load the config file, error: {0}".format(e))
+            print(traceback.format_exc())
+            return False
+        if self.RUNTIME_RE == None or self.PROC_NAME_RE == None or \
+            self.REPO_PATH == None or self.BOT_NAME == None or self.TOKEN == None or\
+            len(self.CENSORING_REPLACE_RE) != len(self.CENSORING_MATCH_RE):
+            print("Configuration is not set up properly. Use the config in the example directory as example.")
+            return False
+        return True
 
     def parse_config(self, text):
         text = text.lstrip() # Keep the spacing on the right
@@ -24,14 +36,10 @@ class Parser:
         match = re.search(r'(.*?) (.*)', text)
         var = match.group(1).upper()
         value = match.group(2)
-        if var == 'FIND_PREF_WARNING_CKEY_RE':
-            self.FIND_PREF_WARNING_CKEY_RE = value
-        elif var == 'FIND_USR_CKEY_RE':
-            self.FIND_USR_CKEY_RE = value
-        elif var == 'FIND_PARAM_CKEY_RE':
-            self.FIND_PARAM_CKEY_RE = value
-        elif var == 'FIND_REF_BASED_CKEY_RE':
-            self.FIND_REF_BASED_CKEY_RE = value
+        if var == 'CENSORING_MATCH_RE':
+            self.CENSORING_MATCH_RE.append(re.compile(value))
+        elif var == 'CENSORING_REPLACE_RE':
+            self.CENSORING_REPLACE_RE.append(value)
         elif var == 'REPO_PATH':
             self.REPO_PATH = value
         elif var == 'BOT_NAME':
@@ -43,9 +51,13 @@ class Parser:
         elif var == 'REOPEN_COMMENT_BASE':
             self.REOPEN_COMMENT_BASE = value.replace("\\n", "\n")
         elif var == 'RUNTIME_RE':
-            self.RUNTIME_RE = value
+            self.RUNTIME_RE = re.compile(value)
         elif var == 'PROC_NAME_RE':
-            self.PROC_NAME_RE = value
+            self.PROC_NAME_RE = re.compile(value)
+        elif var == 'ISSUE_TITLE_BASE':
+            self.ISSUE_TITLE_BASE = value
+        elif var == 'ISSUE_CREATE_BODY':
+            self.ISSUE_CREATE_BODY = value
 
     def parse_file(self, fileName):
         try:
@@ -57,7 +69,7 @@ class Parser:
 
     def parse_text(self, text):
         results = {}
-        match_iter = re.finditer(self.RUNTIME_RE, text)
+        match_iter = self.RUNTIME_RE.finditer(text)
         done = False
         try:
             match = match_iter.__next__()
@@ -75,10 +87,10 @@ class Parser:
                 end_index = len(text)-1
                 done = True
             
-            runtime_title = "[RUNTIME] " + match.group(1)
+            runtime_title = self.ISSUE_TITLE_BASE + match.group(1)
             body = text[start:end_index]
             
-            proc_match = re.search(self.PROC_NAME_RE, body)
+            proc_match = self.PROC_NAME_RE.search(body)
             if(proc_match is not None):
                 runtime_title += " proc: " + proc_match.group(1)
             if(runtime_title not in results):
@@ -88,10 +100,9 @@ class Parser:
         return results
 
     def censor_text(self, text):
-        result = re.sub(self.FIND_PREF_WARNING_CKEY_RE, r'\1CKEY\2', text)
-        result = re.sub(self.FIND_USR_CKEY_RE, r'\1CKEY\2', result)
-        result = re.sub(self.FIND_PARAM_CKEY_RE, r'\1CKEY\2', result)
-        result = re.sub(self.FIND_REF_BASED_CKEY_RE, r'\1CKEY\2CKEY\3', result)
+        result = text
+        for i in range(len(self.CENSORING_MATCH_RE)):
+            result = self.CENSORING_MATCH_RE[i].sub(self.CENSORING_REPLACE_RE[i], result)
         return result
 
     def generate_and_make_github_issues(self, results):
@@ -110,7 +121,7 @@ class Parser:
                 del results[old_issue.title]
         for key in results:
             runt = results[key]
-            self.make_github_issue(repo, runt.runtime_title, runt.body, self.ISSUE_LABELS)
+            self.make_github_issue(repo, runt.runtime_title, self.ISSUE_CREATE_BODY + runt.body, self.ISSUE_LABELS)
             print("opened: " + runt.runtime_title)
         
     def make_github_issue(self, repo, title, body=None, labels=None):
@@ -155,13 +166,13 @@ if len(sys.argv) > 1:
     print(fileName)
 else:
     fileName = "runtime.log"
-    print("Filename defaulted to" + fileName)
-parser = Parser()
-parser.load_config()
-result = parser.parse_file(fileName)
+    print("Filename defaulted to: " + fileName)
 
-if(result is not None):
-    print("Total unique runtimes: " + str(len(result)))
-    parser.generate_and_make_github_issues(result)
-else:
-    print("No runtimes found")
+parser = Parser()
+if parser.load_config():
+    result = parser.parse_file(fileName)
+    if(result is not None):
+        print("Total unique runtimes: " + str(len(result)))
+        parser.generate_and_make_github_issues(result)
+    else:
+        print("No runtimes found")
